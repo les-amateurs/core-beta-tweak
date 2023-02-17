9079fd07c86c0419ae4c943ca2f2f90b4c241d31
Raymomyar <awesomecatstudio@gmail.com>
2023-02-16 22:50:42 -0800

    ooops!

--- vite.config.js
+++ vite.config.js
@@ -40,7 +40,7 @@ export default defineConfig({
               dest: "static/webfonts",
             },
             {
-              src: "./src/webfonts/**/*",
+              src: "./assets/webfonts/**/*",
               dest: "static/webfonts",
             },
             {
